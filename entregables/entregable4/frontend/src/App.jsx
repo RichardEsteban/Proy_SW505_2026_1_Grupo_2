b58c21e2b3f715ec27623b46f15426981e9dcea0
Julio <marquinachahuayoj@gmail.com>
2026-06-24 01:04:25 -0500

    feat: implementar estructura base de frontend y archivos de configuracion docker

---
+++ entregables/entregable4/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Gestión de Inventario y Ventas - Entregable 4</h1>
+    </div>
+  );
+}
+
+export default App;
